2faf19604f80da429f6c7e0e077b3a3bc7fe7838
John Kim <johnkimmsterr@gmail.com>
2021-02-24 18:02:15 -0500

    removed instructions old js and image

--- assets/script.js
+++ assets/script.js
@@ -1,44 +1,25 @@
 const apiKey = "c39c525c5d9e071320ceb9a9d74f8d38";
 
-// working urls to fetch from that are modular
-// var cityURL = 'https://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + apiKey + '&units='+ units;
-
-// fetch(cityURL)
-// .then(function(response){
-//     return response.json();
-// })
-// .then(function(data){
-//     console.log(data)
-// })
-
-
 function getWeather(){
     let userInput = $('#city-input').val().trim();
-    let cityURL = 'https://api.openweathermap.org/data/2.5/weather?q=' + userInput + '&appid=' + apiKey + '&units=imperial';
-    
-    
+    let cityURL = 'https://api.openweathermap.org/data/2.5/weather?q=' + userInput + '&appid=' + apiKey + '&units=imperial';        
     fetch(cityURL)
     .then(function(response){
         return response.json();      
     })
     .then(function(response){
-
         let currentWeatherIcon = 'http://openweathermap.org/img/wn/' + response.weather[0].icon + '@2x.png';
-
-
         let currentTime = response.dt;
-        var convertedTime = new Date(currentTime*1000);
-
-
+        let convertedTime = new Date(currentTime*1000);
+        let formattedTime = moment(convertedTime).format('MM/DD/YYYY, h:mm');
         let currentWeatherCard = `
-        <h2><img src="${currentWeatherIcon}">${response.name} ${convertedTime}</h2>
+        <h2><img src="${currentWeatherIcon}">${response.name} ${formattedTime}</h2>
         <ul class="list-unstyled">
             <li>Temperature: ${response.main.temp}&#8457;</li>
             <li>Humidity: ${response.main.humidity}%</li>
             <li>Wind Speed: ${response.wind.speed} MPH</li>
             <li id="uvIndex"></li>
         </ul>`;
-
         $('#current-weather').html(currentWeatherCard);
 
         let latitude = response.coord.lat;
@@ -50,11 +31,9 @@ function getWeather(){
         })
         .then(function(response){
             $('#uvIndex').html('UV Index: ' + response.value);
-
         })
         getFutureForecast();
-        saveToList();
-       
+        saveToList();       
     })
 };
 
@@ -78,11 +57,12 @@ function getFutureForecast(){
             let forecastWeatherIcon = 'http://openweathermap.org/img/wn/' + response.list[i].weather[0].icon + '@2x.png';
             let forecastDate = response.list[i].dt;
             let unixToDate = new Date(forecastDate*1000);
+            let formattedUnix = moment(unixToDate).format('MM/DD/YYYY');
             let forecastTemp = response.list[i].main.temp;
             let forecastHumidity = response.list[i].main.humidity;
             let forecastCards = `
             <div class = col-2>
-            <h6><img src="${forecastWeatherIcon}">${unixToDate}</h2>
+            <h6><img src="${forecastWeatherIcon}">${formattedUnix}</h2>
             <ul class="list-unstyled">
                 <li>Temperature: ${forecastTemp}&#8457;</li>
                 <li>Humidity: ${forecastHumidity}%</li>
@@ -92,7 +72,7 @@ function getFutureForecast(){
             $('#city-input').val('');
         }
     })
-}
+};
 
 
 var searchedCityList = JSON.parse(localStorage.getItem('searchHistory')) || [];
@@ -103,27 +83,29 @@ function saveToList(){
     localStorage.setItem('searchHistory', JSON.stringify(searchedCityList));
     console.log(searchedCityList);
     renderSearchList();
-}
+};
 
 function renderSearchList(){
     
     searchedCities = JSON.parse(localStorage.getItem('searchHistory'));
     let listInput = $('#city-input').val().trim();
     console.log(searchedCities)
-    // cityList = localStorage.getItem('searchHistory');
-    
-    
+
     $('#city-history').text('');
     
-    
     for (var i = 0; i < searchedCities.length; i++){
-            var newCity = `<button class = btn-secondary cityBtn>${searchedCities[i]}</button>`
-        
-            console.log(searchedCities[i])
+            var newCity = `<button class = "btn-secondary cityBtn">${searchedCities[i]}</button>`;       
             $('#city-history').append(newCity);
             console.log('rendersearchlist');
     }
+    
 };
+
+$('#clear-search-history').on('click', function(){
+    localStorage.clear();
+    $('#city-history').html('');
+});
+
 
 
 function init(){
@@ -135,12 +117,8 @@ function init(){
 
 init();
 
-$('#clear-search-history').on('click', function(){
-    localStorage.clear();
-    $('#city-history').html('');
-})
-
-$('.cityBtn').on('click', function(){
-    
+var cityButton = $('.cityBtn')
+cityButton.on('click', function(){
+    console.log('render city weather info');
+    getWeather();
 });
-
