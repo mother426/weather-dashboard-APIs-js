c16f62ea66e8c2db6b730ee1e939a9598c7d6242
John Kim <johnkimmsterr@gmail.com>
2021-02-24 18:11:17 -0500

    js comments

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,5 @@
 const apiKey = "c39c525c5d9e071320ceb9a9d74f8d38";
-
+// initial function fired when user clicks search button, event listener on line 40
 function getWeather(){
     let userInput = $('#city-input').val().trim();
     let cityURL = 'https://api.openweathermap.org/data/2.5/weather?q=' + userInput + '&appid=' + apiKey + '&units=imperial';        
@@ -41,7 +41,7 @@ $('#search-button').on('click', function(event){
     event.preventDefault();
     getWeather(event);
 });
-
+// Gets future forecast from weather api after searched weather data is loaded 
 function getFutureForecast(){
     let userInput = $('#city-input').val().trim();
     let futureForecastUrl = 'https://api.openweathermap.org/data/2.5/forecast?q=' + userInput + '&units=imperial' + '&appid='+ apiKey;
@@ -51,8 +51,6 @@ function getFutureForecast(){
         return response.json();
     })
     .then (function(response){
-
-        // index positions 3, 11, 19, 27, 35
         for (var i = 3; i < response.list.length; i += 8){
             let forecastWeatherIcon = 'http://openweathermap.org/img/wn/' + response.list[i].weather[0].icon + '@2x.png';
             let forecastDate = response.list[i].dt;
@@ -74,7 +72,7 @@ function getFutureForecast(){
     })
 };
 
-
+// Saves searched cities to user localstorage
 var searchedCityList = JSON.parse(localStorage.getItem('searchHistory')) || [];
 function saveToList(){
     let listInput = $('#city-input').val().trim();
@@ -84,30 +82,24 @@ function saveToList(){
     console.log(searchedCityList);
     renderSearchList();
 };
-
+// renders searched cities as clickable buttons uder search bar
 function renderSearchList(){
-    
     searchedCities = JSON.parse(localStorage.getItem('searchHistory'));
-    let listInput = $('#city-input').val().trim();
-    console.log(searchedCities)
 
-    $('#city-history').text('');
-    
+    console.log(searchedCities)
+    $('#city-history').text('');   
     for (var i = 0; i < searchedCities.length; i++){
             var newCity = `<button class = "btn-secondary cityBtn">${searchedCities[i]}</button>`;       
             $('#city-history').append(newCity);
             console.log('rendersearchlist');
-    }
-    
+    }  
 };
-
+// clear history button event listener
 $('#clear-search-history').on('click', function(){
     localStorage.clear();
     $('#city-history').html('');
 });
-
-
-
+// init ran on line 110, renders searched cities list if previous data exists in localstorage 
 function init(){
     var savedCities = (localStorage.getItem('searchHistory'));
     if (savedCities !== null){
@@ -116,9 +108,9 @@ function init(){
 };
 
 init();
-
+// TODO: make city buttons run the getweather function using this buttons text
 var cityButton = $('.cityBtn')
 cityButton.on('click', function(){
     console.log('render city weather info');
-    getWeather();
+    // getWeather();
 });
